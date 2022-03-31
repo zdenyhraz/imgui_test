574894b7fa66a9e79b21563779b6751c0586f017
zdenyhraz <hrazdira.zdenek@gmail.com>
2022-03-31 22:25:37 +0200

    works!

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <fmt/format.h>
 #include <fmt/ostream.h>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -17,7 +20,6 @@ static void GLFWKeyCallback(GLFWwindow* window, int key, int scancode, int actio
 int main(int argc, char** argv)
 try
 {
-  // std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
   {
     glfwSetErrorCallback(GLFWErrorCallback);
@@ -35,64 +37,66 @@ try
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, GLFWKeyCallback);
 
-    // IMGUI_CHECKVERSION();
-    // ImGui::CreateContext();
-    // ImGuiIO& io = ImGui::GetIO();
-    // (void)io;
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    // io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
-    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
-    // // io.ConfigViewportsNoAutoMerge = true;
-    // // io.ConfigViewportsNoTaskBarIcon = true;
-    // ImGui::StyleColorsDark(); // ImGui::StyleColorsClassic();
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    // ImGuiStyle& style = ImGui::GetStyle();
-    // if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    // {
-    //   style.WindowRounding = 0.0f;
-    //   style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    // }
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+    // io.ConfigViewportsNoAutoMerge = true;
+    // io.ConfigViewportsNoTaskBarIcon = true;
+    ImGui::StyleColorsDark(); // ImGui::StyleColorsClassic();
 
-    // ImGui_ImplGlfw_InitForOpenGL(window, true);
-    // ImGui_ImplOpenGL3_Init("#version 130");
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+      style.WindowRounding = 0.0f;
+      style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
 
     while (!glfwWindowShouldClose(window))
     {
       glfwPollEvents();
-      // ImGui_ImplOpenGL3_NewFrame();
-      // ImGui_ImplGlfw_NewFrame();
-      // ImGui::NewFrame();
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
 
-      // {
-      //   ImGui::Begin("ddx");
-      //   ImGui::Button("Hi mom");
-      //   static float value = 0;
-      //   ImGui::DragFloat("value", &value);
-      //   ImGui::End();
-      // }
+      {
+        ImGui::Begin("ddx");
+        ImGui::Button("Hi mom");
+        static float value = 0;
+        ImGui::DragFloat("value", &value);
+        ImGui::End();
+      }
 
-      // ImGui::Render();
+      ImGui::Render();
       int width, height;
       glfwGetFramebufferSize(window, &width, &height);
       glViewport(0, 0, width, height);
       glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
       glClear(GL_COLOR_BUFFER_BIT);
-      // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-      // if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-      // {
-      //   GLFWwindow* backup_current_context = glfwGetCurrentContext();
-      //   ImGui::UpdatePlatformWindows();
-      //   ImGui::RenderPlatformWindowsDefault();
-      //   glfwMakeContextCurrent(backup_current_context);
-      // }
+      if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+      {
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+      }
 
       glfwSwapBuffers(window);
     }
 
-    // ImGui_ImplOpenGL3_Shutdown();
-    // ImGui_ImplGlfw_Shutdown();
-    // ImGui::DestroyContext();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate();
